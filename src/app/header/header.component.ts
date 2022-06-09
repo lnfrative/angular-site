88b549673f1e5546fe0e72fb7183d028c0bd8498
Christopher Díaz <nealthmc@gmail.com>
2022-06-09 15:47:41 -0500

    [ADD] Header frame

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,17 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
-const someClass = `
-  color: red;
-`
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  someClass = someClass
-
   constructor() { }
 
   ngOnInit(): void {
