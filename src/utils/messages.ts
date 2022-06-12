8682b43bb2485287625d71192be494401b643037
Christopher Díaz <nealthmc@gmail.com>
2022-06-12 01:56:46 -0500

    [UPDATE] free withdrawals feature group

--- src/utils/messages.ts
+++ src/utils/messages.ts
@@ -16,6 +16,16 @@ const messages = {
     ASSETS_WITHOUT_COSTS: 'Deposit more than 50 assets without any associated costs.',
     CHEAP_AND_FAST: 'Cheap and fast conversions',
     CONVERT_ANY_LISTED: 'Convert any listed asset directly from your account.',
+    FREE_WITHDRAWALS: 'Free withdrawals',
+    ANY_CRYPTO_FREE: 'Withdraw any crypto for free! Yes, even Bitcoin*.',
+    AUTO_STAKE: 'Automatically stake all your assets',
+    INCOME_FROM_ALL_ASSETS: 'Get income from all the assets with staking features in your balance.',
+    FAST_WITHDRAWALS: 'Fast withdrawals for purchases',
+    PAY_ANY_CRYPTO_INVOICE: 'Pay any crypto invoice directly from your DogeCash wallet, for free and quickly processed.',
+    DO_MN_WITHOUT_NOTICING: 'Do masternoding without noticing',
+    AUTO_RECEIVE_MN_PROFIT: 'Automatically receive masternodes profit by just depositing coins with masternodes capabilities.',
+    MILITARY_GRADE_SECURITY: 'Military-grade security',
+    ALL_ASSETS_OFFLINE: 'All your assets are hold offline, and your info, AES-256 encrypted.',
   },
 }
 
