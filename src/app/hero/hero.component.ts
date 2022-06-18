7af8f135ec5c20ba0910acd32c1aebdad121243f
Christopher Díaz <nealthmc@gmail.com>
2022-06-18 02:50:09 -0500

    [ADD] Hero animation

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {Component, OnInit, ViewEncapsulation, ViewChild, ElementRef, Inject} from '@angular/core';
+import { DOCUMENT } from '@angular/common'
 
 // utils
 import messages from '../../utils/messages'
@@ -10,11 +11,21 @@ import messages from '../../utils/messages'
   encapsulation: ViewEncapsulation.Emulated,
 })
 export class HeroComponent implements OnInit {
+  scrollAnimationDelay = 4
   messages = messages.en
 
-  constructor() { }
+  @ViewChild('container') container: ElementRef | undefined;
+
+  constructor(@Inject(DOCUMENT) private document: Document) { }
 
   ngOnInit(): void {
+    this.document.addEventListener('scroll', () => {
+      const container = this.container?.nativeElement
+      if (container && window) {
+        const scroll = window.scrollY / this.scrollAnimationDelay
+        container.style.setProperty('--scroll', `${scroll}px`)
+      }
+    })
   }
 
 }
