12e3640039ab79411a9a4f5e466dac5ed587e73d
Christopher Díaz <nealthmc@gmail.com>
2022-06-23 14:08:14 -0500

    [UPDATE] link to terms and conditions page

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+// utils
+import constants from "../../utils/constants";
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  constants = constants
 
   constructor() { }
 
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,6 +2,7 @@ const constants = {
   CLIENTS_APP_URL: 'https://app.dogecash.net',
   CLIENTS_APP_URL_SIGN_UP: 'https://app.dogecash.net/sign-up',
   ROUTE_COMPANY: '/company',
+  ROUTE_TERMS_AND_CONDITIONS: '/terms_and_conditions',
 }
 
 export default constants
